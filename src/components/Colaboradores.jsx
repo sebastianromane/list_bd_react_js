fc723eeeb412dd70c7d2613771527505be580514
sebastianromane <sebastianromane@gmail.com>
2022-09-03 02:02:47 -0400

    collaborating array component

---
+++ src/components/Colaboradores.jsx
@@ -0,0 +1,27 @@
+//collaborating array component
+export const BaseColaboradores = [
+  {
+    id: "1",
+    nombre: "Sebastian Román",
+    correo: "sebastian@desafiolatam.com",
+    completada: false,
+  },
+  {
+    id: "2",
+    nombre: "Valeria Cortés",
+    correo: "valeria@desafiolatam.com",
+    completada: false,
+  },
+  {
+    id: "3",
+    nombre: "Oscar Acevedo",
+    correo: "oscar@desafiolatam.com",
+    completada: false,
+  },
+  {
+    id: "4",
+    nombre: "Juan oyarce",
+    correo: "juan@desafiolatam.com",
+    completada: false,
+  },
+];
